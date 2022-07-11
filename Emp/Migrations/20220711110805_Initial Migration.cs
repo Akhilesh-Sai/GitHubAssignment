debc4f5e3488705842afa1593a9bbc05d9ec912f
Akhilesh <tpvakhilesh@gmail.com>
2022-07-11 17:07:00 +0530

    fourth commit

---
+++ Emp/Migrations/20220711110805_Initial Migration.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Emp.Migrations
+{
+    public partial class InitialMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Gitgit",
+                columns: table => new
+                {
+                    EId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    EName = table.Column<string>(nullable: true),
+                    EmployeeId = table.Column<int>(nullable: false),
+                    EDesign = table.Column<string>(nullable: true),
+                    EDoj = table.Column<DateTime>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Gitgit", x => x.EId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Gitgit");
+        }
+    }
+}
